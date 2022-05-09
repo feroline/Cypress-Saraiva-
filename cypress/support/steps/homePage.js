5a27f3165dfb3433154690d3243a2fb4a8ec2f0a
feroline <92806558+anarochacrp@users.noreply.github.com>
2022-05-09 12:16:18 -0300

    JS ADICIONADO

--- cypress/support/steps/homePage.js
+++ cypress/support/steps/homePage.js
@@ -1,3 +1,13 @@
 import homePage from '../../integration/homePage/homePage.feature';
 
-Given('')
+Given('The user access {string} ##baseURL', (url) => {
+    cy.visit(url)
+    cy.url.should('be.contains', url);
+});
+
+Then('The user should see {string}', (text) => {
+    cy.wrap(cy.get('body')).should('be.contain.text', text);
+});
+
+
+
